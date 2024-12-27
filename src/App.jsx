a562c07f69fc4b6fe4561c152c289f4a561163a1
Panitou <joaco0mr4@gmail.com>
2024-12-27 01:11:05 -0500

    feat: color de pagina principal y nueva card

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import CardUser from "./components/cardUser";
+
+export default function App() {
+  return (
+    <div className="bg-[#0A0A0B] w-screen h-screen text-white font-geo">
+      <CardUser />
+    </div>
+  );
+}
---
+++ src/components/cardUser.jsx
@@ -0,0 +1,7 @@
+export default function cardUser() {
+  return (
+    <div className="bg-[#0F0E10] w-96 h-52 text-3xl font-bold">
+      <h1>Card</h1>
+    </div>
+  );
+}
