efbe7ea9a1eac6ae42b74f3a2b2b536909ef6ae4
Panitou <joaco0mr4@gmail.com>
2025-01-02 13:38:27 -0500

    feat: aumento de padding

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import CardUser from "./components/cardUser";
 
 export default function App() {
   return (
-    <div className="bg-[#0A0A0B] w-screen h-screen text-white font-geo">
+    <div className="bg-[#dfdfff] w-screen h-screen text-white font-geo p-10">
       <CardUser />
     </div>
   );
