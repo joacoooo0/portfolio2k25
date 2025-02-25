abf2e563481aee1ae38b080872805e660ecbdf79
joacoooo0 <joaco0mr4@gmail.com>
2025-02-25 19:33:34 +0100

    imported new fonts

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "@fontsource-variable/geologica/full.css";
+import "@fontsource-variable/rubik";
+import "@fontsource-variable/inter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
