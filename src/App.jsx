84ccc65406b72eda39195f567ccba1e955b7a14b
joacoooo0 <joaco0mr4@gmail.com>
2025-02-25 19:34:26 +0100

    modificated and update components (navbar, cardProject)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import CardUser from "./components/cardUser";
+import Navbar from "./components/navbar";
+import CardProject from "./components/cardProject";
 
 export default function App() {
   return (
-    <div className="bg-[#dfdfff] w-screen h-screen text-white font-geo p-10">
-      <CardUser />
-    </div>
+    <>
+      <div className="absolute flex flex-col justify-center items-center w-full p-9 font-inter">
+        <Navbar />
+        <CardProject />
+      </div>
+      <div className="bg-gradient-to-b from-[#B8E2FF] to-[#F9D6FF] h-screen p-10"></div>
+      <div className="bg-gradient-to-b from-[#F9D6FF] to-[#B8E2FF] h-screen p-10"></div>
+      <div className="bg-gradient-to-b from-[#B8E2FF] to-[#F9D6FF] h-screen p-10"></div>
+    </>
   );
 }
